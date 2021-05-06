6941e3f3af1c6146c5d313f4d80b627b617aa372
JoseSF <0619716120@uma.es>
2021-05-06 19:17:47 +0200

    s

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 app.use(
   "/build/",
   express.static(path.join(__dirname, "node_modules/three/build"))
@@ -12,5 +12,5 @@ app.use(
   "/jsm/",
   express.static(path.join(__dirname, "node_modules/three/examples/jsm"))
 );
-app.get("/", (req, res) => res.render("public/index"));
+
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));

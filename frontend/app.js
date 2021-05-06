7cd4ec73fd3dde553107f60e4b3ea45dccc90583
JoseSF <0619716120@uma.es>
2021-05-06 19:02:51 +0200

    Port changed

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "public"));
 app.use(
   "/build/",
   express.static(path.join(__dirname, "node_modules/three/build"))
@@ -11,5 +12,5 @@ app.use(
   "/jsm/",
   express.static(path.join(__dirname, "node_modules/three/examples/jsm"))
 );
-
-app.listen(8080, () => console.log("Visit http://127.0.0.1:8080"));
+app.get("/", (req, res) => res.render("pages/index"));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
